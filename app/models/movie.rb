52d32597e75b4af69c20fc550ff551fc1e35381f
Mehmet <mehmetkusken@gmail.com>
2024-01-14 18:40:58 +0300

    used Movie api and saved to db

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,30 @@
 class Movie < ApplicationRecord
+
+    def self.film
+        
+        require 'uri'
+        require 'net/http'
+        
+        url = URI("https://imdb-top-100-movies.p.rapidapi.com/")
+        
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        
+        request = Net::HTTP::Get.new(url)
+        request["X-RapidAPI-Key"] = 'cc2eb3e0c1msh74553d800b55ccfp1a8550jsna2c0f4f06fc5'
+        request["X-RapidAPI-Host"] = 'imdb-top-100-movies.p.rapidapi.com'
+        
+        response = http.request(request)
+        json = JSON.parse(response.body)
+        json.each do |d|
+            filmler = Movie.create()
+            filmler.title = d['title']
+            filmler.year = d["year"]
+            filmler.genre = d["genre"]
+            filmler.plot = d["description"]
+            filmler.poster = d["image"]
+            filmler.rating = d["rating"][0]
+            filmler.save
+        end       
+    end
 end
